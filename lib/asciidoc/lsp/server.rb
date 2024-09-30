b6974a17e51ea60b020880ce40863cd987f9b881
Sam Baskinger <basking2@yahoo.com>
2024-09-30 18:23:16 -0500

    Bundle gem initialization.

---
+++ lib/asciidoc/lsp/server.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "server/version"
+
+module Asciidoc
+  module Lsp
+    module Server
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/asciidoc/lsp/server_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Asciidoc::Lsp::Server do
+  it "has a version number" do
+    expect(Asciidoc::Lsp::Server::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
